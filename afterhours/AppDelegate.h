985e054ba519b4e9c2edb60491ddaf308f4bdb8c
Seth Cohen <forwardthinking.llc@gmail.com>
2015-05-18 20:11:37 -0400

    Initial Commit

---
+++ afterhours/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  afterhours
+//
+//  Created by Seth Cohen on 5/18/15.
+//  Copyright (c) 2015 After Hours. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ afterhoursTests/afterhoursTests.m
@@ -0,0 +1,40 @@
+//
+//  afterhoursTests.m
+//  afterhoursTests
+//
+//  Created by Seth Cohen on 5/18/15.
+//  Copyright (c) 2015 After Hours. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface afterhoursTests : XCTestCase
+
+@end
+
+@implementation afterhoursTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
